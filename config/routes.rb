181393f634b92076629589dad893c3573658ec10
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-10-12 18:58:19 +0100

    Bundled

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,6 @@ Rails.application.routes.draw do
   #   end
   resources :restaurants do
     resources :reviews
-  get 'restaurants' => 'restaurants#index'
   end
+  get 'restaurants' => 'restaurants#index'
 end
